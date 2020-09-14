cca3554598e935bdb0e96c9f01f6cf5f77432d1b
peeweetje <petravos70@gmail.com>
2020-09-14 15:05:35 +0200

    Feature: add the images for the pause and medals.

--- Flappy-Bird/assignment1/states/TitleScreenState.lua
+++ Flappy-Bird/assignment1/states/TitleScreenState.lua
@@ -19,7 +19,7 @@ end
 function TitleScreenState:render()
     -- simple UI code
     love.graphics.setFont(flappyFont)
-    love.graphics.printf('Fifty Bird', 0, 64, VIRTUAL_WIDTH, 'center')
+    love.graphics.printf('Peewee Bird', 0, 64, VIRTUAL_WIDTH, 'center')
 
     love.graphics.setFont(mediumFont)
     love.graphics.printf('Press Enter', 0, 100, VIRTUAL_WIDTH, 'center')
